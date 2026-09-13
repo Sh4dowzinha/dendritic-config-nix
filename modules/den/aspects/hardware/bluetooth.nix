38fa030a71fb6ad24eac8c67fea087c3fd569f53
André Fernandes <andrematosf727@gmail.com>
2026-09-13 15:37:07 +0100

    no, I don't want bluetooth enabled at boot

--- modules/den/aspects/hardware/bluetooth.nix
+++ modules/den/aspects/hardware/bluetooth.nix
@@ -1,32 +1,11 @@
 {
   den.aspects.hardware.bluetooth = {
-    nixos =
-      { pkgs, ... }:
-      {
-        hardware.bluetooth = {
-          enable = true;
-          package = pkgs.bluez-experimental;
-          powerOnBoot = true;
-          disabledPlugins = [ "sap" ];
-          settings = {
-            Policy.AutoEnable = true;
-            General = {
-              Privacy = "device";
-              FastConnectable = true;
-              Experimental = true;
-              KernelExperimental = true;
-              JustWorksRepairing = "always";
-              MultiProfile = "multiple";
-              Class = "0x000100";
-              Enable = "Source,Sink,Media,Socket";
-            };
-          };
-        };
-
-        boot.kernelParams = [ "btusb" ];
-
-        services.blueman.enable = true;
+    nixos = {
+      hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = false;
       };
+    };
 
     homeManager =
       { pkgs, ... }:
