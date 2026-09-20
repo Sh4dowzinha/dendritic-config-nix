d5bb77b46bf45ee0aec4cf84dba1594619dbf6ff
André Fernandes <andrematosf727@gmail.com>
2026-09-20 01:53:08 +0100

    use i915 instead of xe for alder lake igpu for now

--- modules/den/aspects/hardware/gpu/intel.nix
+++ modules/den/aspects/hardware/gpu/intel.nix
@@ -12,11 +12,6 @@
         ];
       };
 
-      boot.kernelParams = lib.mkIf (host.gpu.intel.driver == "xe") [
-        "i915.force_probe=!${host.gpu.intel.pci-id}"
-        "xe.force_probe=${host.gpu.intel.pci-id}"
-      ];
-
       environment.sessionVariables = {
         LIBVA_DRIVER_NAME = "iHD";
       };
